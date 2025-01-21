c20d36d8c9bd2bcee084e4d6916c924ceab9201d
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-01-21 18:59:20 +0100

    typoef

---
+++ Test_step/task8/main.js
@@ -0,0 +1,18 @@
+// firstValue jest to pierwsza liczba value
+const firstValue = 10;
+// secondValue , to jest nazwa value
+const secondValue = "Groszek";
+// combineeValue , Jest to kombinacja Value
+const combinedValue = 5 + 'Jestem numerem 1';
+//booleanValue, jest to prawda value
+const booleanValue = true;
+
+console.log(typeof firstValue);
+console.log(typeof secondValue);
+console.log(typeof combinedValue);
+console.log(typeof booleanValue);
+
+console.log( firstValue);
+console.log( secondValue);
+console.log( combinedValue);
+console.log( booleanValue);
