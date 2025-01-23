1214d3960a99d9f44b5d12ef87f85a1046d42ddb
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-01-23 11:32:49 +0100

    List_of_cities (if)

---
+++ Test_step/task11/main.js
@@ -0,0 +1,14 @@
+// zadanie1
+const cities = ["Warsaw",'Berlin','Paris','London','New York' ];
+
+if (cities.includes('Paris')){
+    console.log('Paris is in the list');
+}else {
+    console.log('Paris is not in the list');
+}
+
+if (cities.includes('Tokyo')){
+    console.log('Tokyo is in the list');
+}else {
+    console.log('Tokyo is not in the list');
+}
