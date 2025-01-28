9ec045fcea4c28c301d397bc2dadd0d4cc5ed0dc
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-01-28 16:27:34 +0100

    .

---
+++ Test_step_v2_array/task8/main.js
@@ -0,0 +1,6 @@
+const car = {type:'sedan', color:'green', engine:2.5};
+const carDescription = `${car.type} ${car.color} ${car.engine}`;
+console.log(carDescription);
+const color = {red:100, green:0, blue:50};
+const referenceColor = `${color.red = 50} ${color.green = 50} ${color.blue}`
+console.log(referenceColor);
