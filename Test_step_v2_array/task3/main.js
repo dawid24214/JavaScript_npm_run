778873d8113afd21a03549296f726d8c8cadab39
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-01-28 09:32:49 +0100

    .

---
+++ Test_step_v2_array/task3/main.js
@@ -0,0 +1,8 @@
+
+// Zadanie 3: Sortowanie
+// Stwórz tablicę mixedNumbers z wartościami: 50, 10, 40, 20, 30.
+// Posortuj ją za pomocą metody sort, a następnie wypisz wynik w konsoli.
+//     Zaobserwuj, w jaki sposób JavaScript sortuje liczby (domyślnie jako łańcuchy znaków).
+// Podpowiedź: Możesz dostosować metodę sortującą, aby prawidłowo sortować liczby.
+
+const mixedNumbers = [50, 10, 40, 20, 30];
