8a04bda521f1021338df57086634f2ff00330e4b
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-01-30 17:42:39 +0100

    ~.

---
+++ Test_step_v2_array/task10/main.js
@@ -0,0 +1,7 @@
+const numbersAsNumber = 5;
+const numberAsString = "5";
+
+const equalValueAndType = numbersAsNumber === numberAsString ;
+console.log(equalValueAndType);
+let equalValue = numbersAsNumber !== numberAsString ;
+console.log(equalValue);
