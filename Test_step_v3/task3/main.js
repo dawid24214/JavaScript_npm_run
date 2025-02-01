68b787bfbe287b2d2bf4d6d4e723d3f0f92e4cad
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-01 11:30:45 +0100

    if else

---
+++ Test_step_v3/task3/main.js
@@ -0,0 +1,7 @@
+let liczba = 4;
+
+if (liczba % 2 === 0){
+    console.log('Liczba ' + liczba + ' jest parzysta.');
+}else {
+    console.log('Liczba ' + liczba + ' jest nieparzysta');
+}
---
+++ Test_step_v3/task3/main_v2.js
@@ -0,0 +1,17 @@
+let ocena = 5;
+
+if (ocena === 6){
+    console.log('Celujacy');
+}else if (ocena === 5){
+    console.log('Bardzo dobry');
+}else if (ocena === 4){
+    console.log('Dobry')
+}else if (ocena === 3){
+    console.log('Dostateczny')
+}else if (ocena === 2){
+    console.log('Mierny');
+}else if (ocena === 1){
+    console.log('Niedostateczny');
+}else {
+    console.log('Niepoprawna ocena');
+}
