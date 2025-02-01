7ce8e037ffafa73edc39e2f736ba6ea4439c197d
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-01 11:46:26 +0100

    if and prompt

---
+++ Test_step_v3/task3/main_v3.js
@@ -0,0 +1,9 @@
+let wiek = prompt('Podaj swoj wiek:');
+wiek= parseInt(wiek);
+
+
+if (wiek >= 18) {
+    console.log('Jestes pelnoletni.');
+}else {
+    console.log('nie jestes pelnoletni.');
+}
---
+++ Test_step_v3/task3/main_v4.js
@@ -0,0 +1,9 @@
+let temperaturea = 20;
+
+if (temperaturea < 10){
+    console.log('jest zimno.');
+}else if (temperaturea >= 10 && temperaturea < 25) {
+    console.log('Pogoda jest umiarkowana');
+}else {
+    console.log('Jest goraco');
+}
