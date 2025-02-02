ef00956df8df519bb81b3af72957a2c0a15aac0a
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-02 13:00:31 +0100

    .

--- Test_step_v3/task3/main_v8.js
+++ Test_step_v3/task3/main_v8.js
@@ -1,24 +1,39 @@
-let grade = 1;
+// let grade = 1;
+//
+// switch (grade){
+//     case 1:
+//         console.log('F');
+//         break;
+//     case 2:
+//         console.log('E');
+//         break;
+//     case 3:
+//         console.log('D');
+//         break;
+//     case 4:
+//         console.log('C');
+//         break;
+//     case 5:
+//         console.log('B');
+//         break;
+//     case 6:
+//         console.log('A');
+//         break
+//     default:
+//         console.log('non number');
 
-switch (grade){
-    case 1:
-        console.log('F');
-        break;
-    case 2:
-        console.log('E');
-        break;
-    case 3:
-        console.log('D');
-        break;
-    case 4:
-        console.log('C');
-        break;
-    case 5:
-        console.log('B');
-        break;
-    case 6:
-        console.log('A');
-        break
-    default:
-        console.log('non number');
+
+
+//
+//
+// for (;;){
+//     console.log('To jest petla nieskonczona');
+// }
+
+
+const programmingLanguages = ['JavaScript', 'TypeScript', 'Go' ];
+
+
+for (let counter = 0; counter < programmingLanguages.length; counter++) {
+    console.log(programmingLanguages[counter]);
 }
