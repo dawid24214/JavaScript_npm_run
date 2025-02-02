e113ecaccb2594b00847dec5a870ad1a16b45637
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-02 08:18:43 +0100

    .

---
+++ Test_step_v3/task3/main_v8.js
@@ -0,0 +1,24 @@
+let grade = 1;
+
+switch (grade){
+    case 1:
+        console.log('F');
+        break;
+    case 2:
+        console.log('E');
+        break;
+    case 3:
+        console.log('D');
+        break;
+    case 4:
+        console.log('C');
+        break;
+    case 5:
+        console.log('B');
+        break;
+    case 6:
+        console.log('A');
+        break
+    default:
+        console.log('non number');
+}
