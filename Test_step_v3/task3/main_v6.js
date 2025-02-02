282d253b3c0b78502c1bcc8d377226e227998bec
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-02 07:49:28 +0100

    switch

---
+++ Test_step_v3/task3/main_v6.js
@@ -0,0 +1,28 @@
+let  dzienTygodnia = 1;
+
+switch (dzienTygodnia){
+    case 0:
+        console.log('Niedziela');
+        break;
+    case 1:
+        console.log('Poniedziałek');
+        break;
+    case 2:
+        console.log('Wtorek');
+        break;
+    case 3:
+        console.log('Środa');
+        break;
+    case 4:
+        console.log('Czwartek');
+        break;
+    case 5:
+        console.log('Piątek');
+        break;
+    case 6:
+        console.log('Sobota');
+        break;
+    default:
+        console.log('Niepoprawny numer dnia');
+
+}
