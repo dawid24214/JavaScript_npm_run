9c3fb8b3a13460cb8431e0b6f6a95c0358b84625
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-03 09:35:34 +0100

    Nested loop - iteration over matrix

--- Test_step_v3/task3/main_Petle_v8.js
+++ Test_step_v3/task3/main_Petle_v8.js
@@ -84,3 +84,18 @@
 //         console.log(`Wiersz = ${row}`, `Kolumna=${col}`);
 //     }
 // }
+
+
+// const matrix = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+//     ];
+// for (let i = 0; i < matrix.length; i++){
+//     for (let j = 0; j < matrix[i].length; j++) {
+//         console.log(`Element [${i}][${j}] to : ${matrix[i][j]}` );
+//     }
+// }
+// //Pętla zagnieżdżona – iteracja po macierzy
+
+
