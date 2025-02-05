d2e10f9c6f3f5a794be22671a9260a512315c0e8
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-05 13:00:02 +0100

    .

--- Test_step_v3/task3/main_Petle_v8.js
+++ Test_step_v3/task3/main_Petle_v8.js
@@ -264,3 +264,13 @@
 //     }
 //     console.log(wiersz);
 // }
+
+// let i = 1;
+// while (i <= 3){
+//     let j=  1;
+//     while (j <=3){
+//         console.log(`i = ${i} j = ${j}`);
+//         j++;
+//     }
+//     j++;
+// }
