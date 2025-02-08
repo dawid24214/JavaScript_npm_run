5d51d307ffb14b35c4fb5b65e19723bfa955a666
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-08 16:47:16 +0100

    Update main_Petle_v8.js

--- Test_step_v3/task3/main_Petle_v8.js
+++ Test_step_v3/task3/main_Petle_v8.js
@@ -385,3 +385,14 @@
 // }
 // const result = array(10 , 3);
 // console.log(result);
+function reverseArray(arr){
+    const reverseArray = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reverseArray.push(arr[i]);
+    }
+    return reverseArray
+}
+const numbers = [1, 2, 3, 4, 5];
+const result = reverseArray(numbers);
+console.log(result);
+
