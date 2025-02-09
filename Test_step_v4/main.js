27faf4d194eda5b4eb70ba8492bfc440a10be338
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-09 18:48:11 +0100

    introduction_v2_lvlUP

--- Test_step_v4/main.js
+++ Test_step_v4/main.js
@@ -1,16 +1,32 @@
-const userArray = ['Jan','Ania' , 'Zbyszek', 'Kasia'];
+// const userArray = ['Jan','Ania' , 'Zbyszek', 'Kasia'];
+//
+// const skills = ['JavaScript', 'Python' , 'NodeJS', "Java"];
+//
+// console.log('users:');
+//
+// for (let  i = 0; i < userArray.length ; i++) {
+//
+//     console.log(userArray[i])
+//
+// }
+//
+// console.log('skills:');
+// for (let i = 0 ; i < skills.length; i++) {
+//     console.log(skills[i]);
+// }
 
-const skills = ['JavaScript', 'Python' , 'NodeJS', "Java"];
+
+function printArrayValues(array){
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
+
+const userArray = ['Jan', 'Ania', 'Zbyszek' , 'Kasia'];
+const skills = ['JavaScript' , 'Python' , 'NodeJS' , 'Java'];
 
 console.log('users:');
+printArrayValues(userArray);
 
-for (let  i = 0; i < userArray.length ; i++) {
-
-    console.log(userArray[i])
-
-}
-
-console.log('skills:');
-for (let i = 0 ; i < skills.length; i++) {
-    console.log(skills[i]);
-}
+console.log(skills);
+printArrayValues(skills);
