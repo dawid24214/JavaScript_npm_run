d6207e6f03aabd7506c6f0ecdffd0f57a4b112d0
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-02-09 18:50:40 +0100

    .

--- Test_step_v4/main.js
+++ Test_step_v4/main.js
@@ -15,18 +15,26 @@
 //     console.log(skills[i]);
 // }
 
+//
+// function printArrayValues(array){
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(array[i]);
+//     }
+// }
+//
+// const userArray = ['Jan', 'Ania', 'Zbyszek' , 'Kasia'];
+// const skills = ['JavaScript' , 'Python' , 'NodeJS' , 'Java'];
+//
+// console.log('users:');
+// printArrayValues(userArray);
+//
+// console.log(skills);
+// printArrayValues(skills);
 
-function printArrayValues(array){
-    for (let i = 0; i < array.length; i++) {
-        console.log(array[i]);
-    }
-}
 
-const userArray = ['Jan', 'Ania', 'Zbyszek' , 'Kasia'];
-const skills = ['JavaScript' , 'Python' , 'NodeJS' , 'Java'];
 
-console.log('users:');
-printArrayValues(userArray);
-
-console.log(skills);
-printArrayValues(skills);
+//Function / Method
+// Functions are one of the basic "building blocks" in JavaScript.
+// A function is a set of expressions/statements that perform some task or calculate a value.
+// To use a function, you must first
+//define it somewhere in the scope from which it will be called.
