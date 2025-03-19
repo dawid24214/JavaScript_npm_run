80c63f5c26551dc8e92d0d18d464bf84b21dbc37
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-03-19 10:28:54 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ function App() {
 
   return (
       <div className='body'>
-          <p className="first">To jest pierwszy paragraf.</p>
-          <p className="second">To jest drugi paragraf.</p>
-          <span className="highlight">To jest span.</span>
+          {/*<p className="first">To jest pierwszy paragraf.</p>*/}
+          {/*<p className="second">To jest drugi paragraf.</p>*/}
+          {/*<span className="highlight">To jest span.</span>*/}
+
+
+          <h1>Witaj na stronie!</h1>
+          <a href='#'>Kliknij mnie </a>
+          <button className='cta-button'>Kliknij mnie </button>
 
       </div>
   )
