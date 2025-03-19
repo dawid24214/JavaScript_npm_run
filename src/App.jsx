758057cdff01102f0b9fb2897ea0d325aee5ea50
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-03-19 10:49:25 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,11 @@ function App() {
 
   return (
       <div className='body'>
-          {/*<p className="first">To jest pierwszy paragraf.</p>*/}
-          {/*<p className="second">To jest drugi paragraf.</p>*/}
-          {/*<span className="highlight">To jest span.</span>*/}
-
-
-          <h1>Witaj na stronie!</h1>
-          <a href='#'>Kliknij mnie </a>
-          <button className='cta-button'>Kliknij mnie </button>
-
+          <section>
+              <article>
+                  <div>Dawid Grochowski</div>
+              </article>
+          </section>
       </div>
   )
 }
